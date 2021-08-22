955519b36c95cdf4dfd40829083c0f9a107ad7f1
Mattun1212 <kouta.dvlp@icloud.com>
2021-08-23 01:17:03 +0900

    accelerometer completed

---
+++ Accelerometer/GameViewController.swift
@@ -0,0 +1,60 @@
+//
+//  GameViewController.swift
+//  Accelerometer
+//
+//  Created by Koutaro Matsushita on 2021/08/22.
+//
+
+import UIKit
+import CoreMotion
+
+class GameViewController: UIViewController {
+
+    @IBOutlet var awaImageView: UIImageView!
+    let motionManager = CMMotionManager()
+    var accelerationX: Double = 0.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if motionManager.isAccelerometerActive{
+            motionManager.accelerometerUpdateInterval = 0.01
+            motionManager.startAccelerometerUpdates(to: OperationQueue.current!){ data, error in
+                
+                self.accelerationX = (data?.acceleration.x)!
+                self.awaImageView.center.x += CGFloat(self.accelerationX*20)
+                
+                if self.awaImageView.frame.origin.x < 32{
+                    self.awaImageView.frame.origin.x = 32
+                }
+                
+                if self.awaImageView.frame.origin.x > 304{
+                    self.awaImageView.frame.origin.x = 304
+                }
+            }
+            
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.accelerationX = self.accelerationX
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
